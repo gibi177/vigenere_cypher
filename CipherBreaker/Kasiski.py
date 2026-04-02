11063125d8f4cae082a2bedffebdd1deb6395396
Felipe Gibin <129801092+gibi177@users.noreply.github.com>
2026-04-02 12:40:36 -0300

    refactor(kasiski): create public interface and make helper methods private

--- CipherBreaker/Kasiski.py
+++ CipherBreaker/Kasiski.py
@@ -2,7 +2,7 @@ class Kasiski:
   def __init__(self, text: str):
     self.text = text
 
-  def clean_text(self) -> str:
+  def _clean_text(self) -> str:
     result = ""
 
     for ch in self.text:
@@ -11,15 +11,14 @@ class Kasiski:
 
     return result
 
-  def get_substrings(self, clean_text: str, length: int) -> list[str]:
+  def _get_substrings(self, clean_text: str, length: int) -> list[str]:
     substrings = []
     for i in range(len(clean_text) - length + 1):
       substrings.append(clean_text[i:i+length])
 
     return substrings
 
-  def substring_count(self, clean_text: str, length: int) -> dict[str, int]:
-    substrings = self.get_substrings(clean_text, length)
+  def _substring_count(self, substrings: list[str]) -> dict[str, int]:
     substrings_count = {} # Dictionary where keys are substrings and values are how many times that substring appears in the text
 
     for substring in substrings:
@@ -30,7 +29,7 @@ class Kasiski:
 
     return substrings_count
 
-  def repeated_substrings_positions(self, clean_text: str, substrings_count: dict[str, int]) -> dict[str, list[int]]:
+  def _repeated_substrings_positions(self, clean_text: str, substrings_count: dict[str, int]) -> dict[str, list[int]]:
     result = {} # Dictionary where keys are substrings and values are lists of positions where that substring appears in the text
 
     for substring, count in substrings_count.items():
@@ -51,7 +50,7 @@ class Kasiski:
     
     return result
 
-  def repeated_substrings_distances(self, substrings_positions: dict[str, list[int]]) -> dict[str, list[int]]:
+  def _repeated_substrings_distances(self, substrings_positions: dict[str, list[int]]) -> dict[str, list[int]]:
     distances = {} # Dictionary where keys are substrings and values are lists of distances between the positions where that substring appears
 
     for substring, positions in substrings_positions.items():
@@ -64,7 +63,7 @@ class Kasiski:
 
     return distances
 
-  def distances_gcd(self, distances: dict[str, list[int]]) -> dict[str, int]:
+  def _distances_gcd(self, distances: dict[str, list[int]]) -> dict[str, int]:
     from math import gcd
     from functools import reduce
     
@@ -76,7 +75,7 @@ class Kasiski:
 
     return gcds
 
-  def likely_key_length(self, distances_gcds: dict[str, int]) -> int:
+  def _likely_key_length(self, distances_gcds: dict[str, int]) -> int:
     
     freq = {}  # key = GCD, value = count
     for gcd_value in distances_gcds.values():
@@ -85,43 +84,42 @@ class Kasiski:
       else:
         freq[gcd_value] = 1
 
+    if not freq:
+      return 0
+      
     most_common_gcd = max(freq, key=freq.get)
     return most_common_gcd
 
+  def find_key_length(self, substring_length: int) -> int:
+    
+    # 1. Clean the text
+    clean = self._clean_text()
+
+    # 2. Get all substrings of length substring_length
+    substrings = self._get_substrings(clean, substring_length)
+
+    # 3. Count how many times each substring appears
+    counts = self._substring_count(substrings)
+
+    # 4. Get positions of repeated substrings
+    positions = self._repeated_substrings_positions(clean, counts)
+
+    # 5. Calculate distances between repeated substrings
+    distances = self._repeated_substrings_distances(positions)
+
+    # 6. Compute GCDs of distances
+    gcds = self._distances_gcd(distances)
+
+    # 7. Determine most likely key length
+    key_length = self._likely_key_length(gcds)
+
+    return key_length
+    
 
 
 if __name__ == "__main__":  
-  text = "Abcabcabc"
-  print(f"Cipher text: {text}")
-  k = Kasiski(text)
-
-  # 1. Clean the text
-  clean = k.clean_text()
-  print("Clean text:", clean)
-  print("\n")
-
-  # 2. Get all substrings of length 3
-  substrings = k.get_substrings(clean, 3)
-  print("Substrings of length 3:", substrings)
-
-  # 3. Count how many times each substring appears
-  counts = k.substring_count(clean, 3)
-  print("Substring counts:", counts)
-  print("\n")
-
-  # 4. Get positions of repeated substrings
-  positions = k.repeated_substrings_positions(clean, counts)
-  print("Repeated substring positions:", positions)
-
-  # 5. Calculate distances between repeated substrings
-  distances = k.repeated_substrings_distances(positions)
-  print("Distances between repeated substrings:", distances)
-  print("\n")
-
-  # 6. Compute GCDs of distances
-  gcds = k.distances_gcd(distances)
-  print("GCDs of distances:", gcds)
-
-  # 7. Determine most likely key length
-  key_length = k.likely_key_length(gcds)
-  print("Most likely key length (most common gcd):", key_length)
+  cipher_text = "Abcabcabc"
+  k = Kasiski(cipher_text)
+  prob_key = k.find_key_length(3)
+  print(prob_key)
+  
